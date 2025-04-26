8dbada35c05d770ea27c8d160103d97d079cc7f3
MF366 <real_mf366@yahoo.com>
2025-04-26 18:45:59 +0100

    Added browser utils

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # WriterSharp
-Writer# (or WriterSharp) is THE new WriterClassic, now in C#, with even more corss-platform compatibility.
+<img src="Assets/Logo.png" align="right" width="100" alt="Norb's WriterClassic Logo"/>
+
+So, what is WriterSharp?
+
+Well, long time ago *(it was only 3 years)*, I created a Python program with a GUI called [**WriterClassic**](https://github.com/MF366-Coding/WriterClassic). It was a basic text editor that evolved into something special - at least for me.
+
+It was my biggest project for quite a while and it was also... my first actual programming project. I mean, public at least.
+
+I ended up releasing 11 major versions for it and almost released v12.0.0, but I couldn't. The codebase was old and unorganized and it was getting increasingly hard to maintain WriterClassic.
+
+So, **WriterSharp** was born. WriterSharp (please don't write **Writer#** - it's cursed) is WriterClassic but in C#, with a more modern look and better cross-platform compatibility.
+
+I also made the decision of not dropping the WriterClassic logo for a new one, because **A) It was made by a friend** and **B) It's *classic* :)**. 
---
+++ Utils/Browser.cs
@@ -0,0 +1,93 @@
+﻿/*
+ *  ___                                       
+ * | _ )  _ _   ___  __ __ __  ___  ___   _ _ 
+ * | _ \ | '_| / _ \ \ V  V / (_-< / -_) | '_|
+ * |___/ |_|   \___/  \_/\_/  /__/ \___| |_|  
+ *                 
+ * MIT License * MF366
+ *                 
+ */
+
+
+// System
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading.Tasks;
+
+
+namespace WriterSharp.Browser
+{
+
+	/// <summary>
+	/// Browser-related operations.
+	/// </summary>
+	public static class BrowserService
+	{
+
+		/// <summary>
+		/// Open a URL with the default webbrowser.
+		/// </summary>
+		/// <param name="url">The URL to open</param>
+		/// <returns>0 if successful or -1 if an error was caught</returns>
+		public static int OpenURL(string url)
+		{
+
+			try
+			{
+
+				ProcessStartInfo psi = new()
+				{
+
+					FileName = url,
+					UseShellExecute = true
+
+				};
+				Process.Start(psi);
+				return 0;
+
+			}
+
+			catch (Exception)
+			{
+
+				return -1;
+
+			}
+
+		}
+
+		/// <summary>
+		/// Opens a URL with the default webbrowser, but asyncronously.
+		/// </summary>
+		/// <param name="url">The URL to open.</param>
+		/// <returns>Null if an error was caught</returns>
+		public static async Task<Process?> OpenURLAsync(string url)
+		{
+
+			try
+			{
+
+				ProcessStartInfo psi = new()
+				{
+
+					FileName = url,
+					UseShellExecute = true
+
+				};
+
+				return await Task.Run(() => Process.Start(psi));
+
+			}
+			catch (Exception)
+			{
+
+				return null;
+
+			}
+
+		}
+
+	}
+
+}
